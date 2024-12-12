660925de7a753c698fdeb7a50b440557f631f5a4
Kexi Dang <dangkexi@gmail.com>
2024-12-12 10:30:21 +0800

    day11 commit

---
+++ day-11.ts
@@ -0,0 +1,35 @@
+import type { Expect, Equal } from 'type-testing';
+
+// The `new` keyword here is used to define the shape of a constructor function type.
+type Excuse<T extends Record<string, string>> = new (props: T) => ({
+	[K in keyof T]: `${K & string}: ${T[K]}`;
+}[keyof T]);
+
+const existingExcuses = {
+  karaoke: ['Kendrick Lamar, Opeth'],
+  margarita: 'Peppermint-Jalapeño Mojito',
+}
+
+const helpingTheReindeer = {
+  helping: 'the reindeer',
+} as const;
+declare const Excuse0: Excuse<typeof helpingTheReindeer>;
+const excuse0 = new Excuse0({
+  ...existingExcuses,
+  ...helpingTheReindeer,
+});
+type t0_actual = typeof excuse0;
+type t0_expected = "helping: the reindeer";
+type t0 = Expect<Equal<t0_actual, t0_expected>>;
+
+const eatingFudge = {
+  eating: 'fudge',
+} as const;
+declare const Excuse1: Excuse<typeof eatingFudge>;
+const excuse1 = new Excuse1({
+  ...existingExcuses,
+  ...eatingFudge,
+});
+type t1_actual = typeof excuse1;
+type t1_expected = "eating: fudge";
+type t1 = Expect<Equal<t1_actual, t1_expected>>;
