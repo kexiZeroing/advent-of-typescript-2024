293b013ecdc3c05f01329314e7636874a7fc5c27
Kexi Dang <dangkexi@gmail.com>
2024-12-17 18:02:06 +0800

    day17 commit

--- day-17.ts
+++ day-17.ts
@@ -13,9 +13,8 @@ type FirstChar<T extends string> = T extends `${infer U}${string}` ? U : never;
 type FirstItem<T extends any[]> = T extends [infer U, ...any[]] ? U : never;
 
 const firstChar = <const T extends string>(x: T) => x[0] as FirstChar<T>;
-const firstItem = <const T extends any[]>(x: T): T extends [] ? never : T[0] => x[0];
-
-// const firstItem = <const T extends any[]>(x: T): T[0] => x[0] as FirstItem<T>;
+const firstItem = <const T extends any[]>(x: T): T[0] => x[0] as FirstItem<T>;
+//const firstItem = <const T extends any[]>(x: T): T extends [] ? never : T[0] => x[0];
 
 const makeTuple = <T>(x: T): [T] => [x];
 const makeBox = <T>(value: T): { value: T } => ({ value });
@@ -24,17 +23,16 @@ const makeBox = <T>(value: T): { value: T } => ({ value });
 import type { Equal, Expect } from 'type-testing';
 
 const t0 = compose(upperCase, makeTuple, makeBox)("hello!").value[0];
-//    ^?
-type t0_actual = typeof t0;    // =>
-type t0_expected = "HELLO!"; // =>
+type t0_actual = typeof t0;    
+type t0_expected = "HELLO!"; 
 type t0_test = Expect<Equal<t0_actual, t0_expected>>;
 
 const t1 = compose(makeTuple, firstItem, makeBox)("hello!" as const).value;
-type t1_actual = typeof t1;    // =>
-type t1_expected = "hello!"; // =>
+type t1_actual = typeof t1;    
+type t1_expected = "hello!"; 
 type t1_test = Expect<Equal<t1_actual, t1_expected>>;
 
 const t2 = compose(upperCase, firstChar, lowerCase)("hello!");
-type t2_actual = typeof t2;    // =>
-type t2_expected = "h"; // =>
+type t2_actual = typeof t2;    
+type t2_expected = "h"; 
 type t2_test = Expect<Equal<t2_actual, t2_expected>>;
